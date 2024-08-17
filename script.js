5fa2fe3d4282b941b904687740329a23fd6e112b
RodrigoSuar <rodrigobenjaminsuarez@gmail.com>
2024-08-17 13:00:37 -0500

    added js to make the game more interactive

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const numButton = 3;
+const buttonArray = [];
+
+
+
 // Function to get the computer's choice
 function getComputerChoice() {
     const choices = ['rock', 'paper', 'scissors'];
@@ -21,13 +26,44 @@ function determineWinner(playerChoice, computerChoice) {
 }
 
 // Function to play the game
-function playGame() {
-    const playerChoice = prompt("Enter your choice (rock, paper, or scissors):").toLowerCase();
+function playGame(playerChoice) {
     const computerChoice = getComputerChoice();
     console.log(`You chose: ${playerChoice}\nComputer chose: ${computerChoice}`);
-    console.log(determineWinner(playerChoice, computerChoice));
+    diplayWinner(determineWinner(playerChoice, computerChoice));
+}
+
+
+function diplayWinner(winner){
+    let element = document.querySelector('.text');
+    element.textContent = winner;
+}
+
+function createButton(){
     
+    const buttonContainer = document.querySelector(".buttonContainer");
+
+    for(let i = 0; i <numButton; i++){
+        const button = document.createElement('button')
+        let choice;
+        if (i == 0){
+            choice = "rock";
+            button.innerText = "Rock"
+            button.className = "Rock"
+        }else if (i == 1){
+            choice = "paper";
+            button.innerText = "Paper"
+            button.className = "Paper"
+        } else{
+            choice = "scissors";
+            button.innerText = "Scissors"
+            button.className = "Scissors"
+        }
+        button.addEventListener("click", () => playGame(choice));
+        buttonContainer.appendChild(button);
+        buttonArray.push(button);
+    }
 }
 
 // Start the game
-playGame();
+createButton();
+
